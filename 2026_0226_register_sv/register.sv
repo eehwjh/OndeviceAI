847c4cb41494d83d00ce99f64a1f044a865018dd
eehwjh <ahnjeonghyeon98@gmail.com>
2026-02-26 18:37:59 +0900

    Add files via upload

---
+++ 2026_0226_register_sv/register.sv
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+
+module register_8bit (
+    input              clk,
+    input              rst,
+    input  logic       we,
+    input  logic [7:0] wdata,
+    output logic [7:0] rdata
+);
+
+    always_ff @(posedge clk, posedge rst) begin : blockName
+        if (rst) begin
+            rdata <= 0;
+        end else begin
+            if (we) begin
+                rdata <= wdata;
+            end
+        end
+    end
+
+endmodule
