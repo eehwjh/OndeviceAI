2e3cb30fa881560a0300c0774a420330d6ca726d
eehwjh <ahnjeonghyeon98@gmail.com>
2026-03-10 12:56:52 +0900

    Add files via upload

---
+++ RV32I_RSI /sim/tb_rv32i.sv
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+
+module tb_rv32i ();
+    logic clk, rst;
+
+    rv32i_top dut(.*);
+
+    always #5 clk = ~clk;
+
+    initial begin
+        clk = 0;
+        rst = 1;
+
+        @(negedge clk);
+        @(negedge clk);
+        rst = 0;
+
+        repeat(3) @(negedge clk);
+        $stop;
+    end
+endmodule
